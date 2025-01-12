713f7389dacdb480f2267894d78013c3b177a2c1
今天吃没吃123 <13580127+have-you-eaten-today-123@user.noreply.gitee.com>
2025-01-12 09:40:45 +0800

    feat:初始化项目

---
+++ aiWx/App.vue
@@ -0,0 +1,27 @@
+<style lang="scss">
+/* 注意要写在第一行，同时给style标签加入lang="scss"属性 */
+@import "uview-ui/index.scss";
+</style>
+
+<script>
+export default {
+	onLaunch: function () {
+		console.log('App Launch')
+	},
+	onShow: function () {
+		console.log('App Show')
+	},
+	onHide: function () {
+		console.log('App Hide')
+	}
+}
+</script>
+
+<style>
+/*每个页面公共css */
+.container{
+	height: 100rpx;
+	background-color: #e9e9e9;
+	font-size:30rpx;
+}
+</style>
