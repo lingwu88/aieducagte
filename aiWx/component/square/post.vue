1c249f0d5d3b0e409a3438fe4b2d69f9f78b7493
PennLam <lemonmonlucy@163.com>
2025-02-27 23:06:56 +0800

    update

--- aiWx/component/square/post.vue
+++ aiWx/component/square/post.vue
@@ -2,20 +2,22 @@
   <view>
     <view class="container">
       <view class="header">
-        <u--image src="/static/my/user.png" class="user-img" height="175rpx" width="175rpx"  shape="circle"></u--image>
+        <image :src="img" class="user-img" height="175rpx" width="175rpx"  shape="circle"></image>
         <view class="header-content">
           <text class="name" selectable="false" space="false" decode="false">{{ author }}</text>
           <text class="header-content-item" selectable="false" space="false" decode="false">{{ time }}</text>
-          <text class="header-content-item" selectable="false" space="false" decode="false">来自:{{ sourceType }}</text>
+          <!-- <text class="header-content-item" selectable="false" space="false" decode="false">来自{{ sourceType }}</text> -->
         </view>
       </view>
+      <view class="header-title" selectable="false" space="false" decode="false">{{ title }}</view>
       <view class="content">{{ content }}</view>
       <view class="footer">
-        <icon img="/static/square/read.png"></icon>
-        <icon img="/static/square/comment.png" @click="handleShow"></icon>
+        <icon :img="showStar?'/static/square/star-fill.png':'/static/square/star.png'" @click="handleStar" :number="likeCount"></icon>
+        <!-- <icon img="/static/square/read.png" :number="likeCount"></icon> -->
+        <icon img="/static/square/comment.png" :number="commentCount" @click="handleShow"></icon>
       </view>
     </view>
-    <commentSection v-if="showComment"></commentSection>
+    <commentSection v-if="showComment" :commentList="commentList"></commentSection>
   </view>
     
 </template>
@@ -29,6 +31,18 @@ export default{
     commentSection
   },
   props:{
+    likeCount:{
+      type:Number,
+      required:true
+    },
+    commentCount:{
+      type:Number,
+      required:true
+    },
+    img:{
+      type:String,
+      required:true
+    },
     time:{
       type:String,
       required:true
@@ -43,23 +57,42 @@ export default{
     },
     sourceType:{
       type:String,
+      required:false
+    },
+    title:{
+      type:String,
+      required:true
+    },
+    commentList:{
+      type:Array,
+      default:[]
+    },
+    showStar:{
+      type:Boolean,
       required:true
     }
   },
   data(){
     return{
-      item:{
-        name:"lingwu99",
-        time:"2025-1-20 19:00:00",
-        type:"Ai agent"
-      },
-      showComment:false
+      // item:{
+      //   name:"lingwu99",
+      //   time:"2025-1-20 19:00:00",
+      //   type:"Ai agent"
+      // },
+      showComment:false,
     }
   },
   methods: {
+    handleStar(){
+      console.log(this.showStar);
+      this.$emit('star',this.showStar)
+    },
     handleShow(){
       this.showComment = !this.showComment
-      this.$emit('showComment')
+      if(this.showComment){
+        this.$emit('comment',this.showComment)
+      }
+      this.$emit('showComment',false)
     }
   },
 }
@@ -86,16 +119,23 @@ export default{
     width: 100%;
 
     .user-img{
-      width: 60rpx;
-      height: 60rpx;
+      width: 120rpx;
+      height: 120rpx;
+      border-radius: 60%;
     }
     &-content{
+      margin-left: 30rpx;
+      margin-top:20rpx;
       display: flex;
       flex-direction: column;
-      justify-content: space-around;
+      justify-content: space-evenly;
       align-items: flex-start;
       flex:1;
       height:200rpx;
+    }
+    &-title{
+      font-size: 40rpx;
+      padding: 0 0 0 60rpx;
     }
   }
 
