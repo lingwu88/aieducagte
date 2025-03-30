dad9fd3530a95ab43ce624fae4e8355c6dbf6deb
PennLam <lemonmonlucy@163.com>
2025-03-30 14:55:49 +0800

    延迟加载机制

--- aiWx/pages/progressFeedback/learningSituation.vue
+++ aiWx/pages/progressFeedback/learningSituation.vue
@@ -5,7 +5,7 @@
         :scrollable="false"
         :list="list4"
         lineWidth="30"
-        lineColor="#f56c6c"
+        lineColor="#E483AA"
         :activeStyle="{
             color: '#303133',
             fontWeight: 'bold',
@@ -18,7 +18,19 @@
         itemStyle="padding-left: 15px; padding-right: 15px; height: 34px;"
         >
         </u-tabs>
-      <l-echart ref="chartRef" @finished="setChart"></l-echart>
+        
+      <!-- 添加加载状态 -->
+      <view class="loading-container" v-if="loading">
+        <view class="loading-spinner">
+          <view class="spinner"></view>
+        </view>
+        <view class="loading-text">数据加载中...</view>
+      </view>
+      
+      <!-- 数据加载完成后显示图表 -->
+      <view class="chart-container" v-else>
+        <l-echart ref="chartRef" @finished="setChart"></l-echart>
+      </view>
     </view>
   </view>
 </template>
@@ -31,10 +43,19 @@ export default{
 		},
   onLoad(){
     this.userId = uni.getStorageSync('userId')
-    this.getData()
+  },
+  onShow(){
+    // 添加延迟加载
+    this.loading = true;
+    
+    // 使用setTimeout模拟加载过程
+    setTimeout(() => {
+      this.getData();
+    }, 800);
   },
   data() {
     return {
+      loading: true, // 添加加载状态控制
       list4: [
           {
               name: '每天'
@@ -71,14 +92,21 @@ export default{
               value:Math.floor(item*100/sum),
               name:this.typeList[index]
             }})
+            
+            // 数据加载完成后，设置loading为false
+            this.loading = false;
           })
           .catch(err=>{
             console.log(err);
-            
+            // 即使加载失败也要关闭loading状态
+            this.loading = false;
           })
           
         },
         async setChart(customOption) {
+            // 如果数据还在加载中，不初始化图表
+            if (this.loading) return;
+            
             // chart 图表实例不能存在data里
             const chart = await this.$refs.chartRef.init(echarts);
            let defaultoption={
@@ -162,15 +190,54 @@ export default{
             // })
             // chart2.setOption(this.option2)
         },
-    },
-    onShow(){
-      // this.setData()
     }
 }
 </script>
 
 <style scoped lang="scss">
 .box{
-  margin:auto 10rpx ;
+  margin:auto 10rpx;
+}
+
+.loading-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 400rpx;
+}
+
+.loading-spinner {
+  width: 80rpx;
+  height: 80rpx;
+  margin-bottom: 20rpx;
+}
+
+.spinner {
+  width: 100%;
+  height: 100%;
+  border: 6rpx solid rgba(228, 131, 170, 0.2);
+  border-top-color: #E483AA;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.loading-text {
+  font-size: 28rpx;
+  color: #666;
+}
+
+.chart-container {
+  margin: 20rpx 0;
+  padding: 20rpx;
+  background-color: #ffffff;
+  border-radius: 12rpx;
+  box-shadow: 0 4rpx 12rpx rgba(0, 0, 0, 0.05);
 }
 </style>
