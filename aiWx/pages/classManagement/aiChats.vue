5ad1711843e3c01291895f3035325c53eae0d8dd
PennLam <lemonmonlucy@163.com>
2025-03-30 15:21:59 +0800

    润色aichat

--- aiWx/pages/classManagement/aiChats.vue
+++ aiWx/pages/classManagement/aiChats.vue
@@ -8,19 +8,17 @@
 
     <!-- 中间搜索框 -->
     <view class="search-box">
-      <textarea
+      <input
         v-model="inputText"
         :adjust-position="false"
         :cursor-spacing="20"
-        auto-height
-        :show-confirm-bar="false"
         placeholder="请输入您的学习需求或关键词..."
         :maxlength="-1"
         @focus="handleFocus"
         @blur="handleBlur"
-        class="search-textarea"
+        class="search-input"
       />
-      
+      <view class="divider"></view>
       <!-- 规划按钮 -->
       <view 
         class="search-btn"
@@ -32,12 +30,16 @@
     </view>
 
     <!-- 模式选择 -->
-    <view class="mode-selector">
-      <view class="mode-item active">
-        <view class="mode-text">普通模式</view>
-      </view>
-      <view class="mode-item" @click="handleToProfession">
-        <view class="mode-text">专业模式</view>
+    <view class="mode-switch">
+      <view class="mode-label">选择模式：</view>
+      <view class="mode-options">
+        <view class="mode-option" :class="{ active: !isProfessionalMode }">
+          <text>普通模式</text>
+        </view>
+        <view class="mode-option" :class="{ active: isProfessionalMode }" @click="handleToProfession">
+          <text>专业模式</text>
+          <text class="mode-desc">适用于更复杂的学习规划</text>
+        </view>
       </view>
     </view>
   </view>
@@ -56,6 +58,7 @@ export default {
   data() {
     return {
       inputText: '',
+      isProfessionalMode: false
     }
   },
   methods: {
@@ -102,7 +105,7 @@ export default {
   .header {
     text-align: center;
     margin-bottom: 60rpx;
-    margin-top: 60rpx;
+    margin-top: 100rpx;
     
     .title {
       font-size: 48rpx;
@@ -119,59 +122,80 @@ export default {
   
   .search-box {
     background-color: #fff;
-    border-radius: 20rpx;
-    padding: 30rpx;
+    border-radius: 45rpx;
+    padding: 0 30rpx;
     margin-bottom: 40rpx;
     box-shadow: 0 4rpx 20rpx rgba(0, 0, 0, 0.05);
+    display: flex;
+    align-items: center;
+    height: 110rpx;
     
-    .search-textarea {
-      width: 100%;
-      min-height: 200rpx;
+    .search-input {
+      flex: 1;
+      height: 90rpx;
       font-size: 32rpx;
-      line-height: 1.5;
-      margin-bottom: 30rpx;
+    }
+    
+    .divider {
+      width: 2rpx;
+      height: 50rpx;
+      background-color: #eee;
+      margin: 0 20rpx;
     }
     
     .search-btn {
-      width: 100%;
-      height: 90rpx;
-      line-height: 90rpx;
-      text-align: center;
-      background: linear-gradient(to right, #5b6af0, #7b89ff);
-      color: #fff;
-      border-radius: 45rpx;
-      font-size: 34rpx;
-      opacity: 0.5;
+      color: #5b6af0;
+      font-size: 32rpx;
+      padding: 0 10rpx;
       
       &.active {
-        opacity: 1;
+        font-weight: 500;
       }
     }
   }
   
-  .mode-selector {
-    display: flex;
-    justify-content: center;
-    margin-top: 20rpx;
+  .mode-switch {
+    margin-top: 40rpx;
     
-    .mode-item {
-      padding: 20rpx 40rpx;
-      margin: 0 20rpx;
-      border-radius: 40rpx;
-      background-color: #fff;
-      box-shadow: 0 2rpx 10rpx rgba(0, 0, 0, 0.05);
+    .mode-label {
+      font-size: 28rpx;
+      color: #999;
+      margin-bottom: 20rpx;
+    }
+    
+    .mode-options {
+      display: flex;
+      flex-direction: column;
       
-      &.active {
-        background: linear-gradient(to right, #5b6af0, #7b89ff);
+      .mode-option {
+        display: flex;
+        flex-direction: column;
+        padding: 20rpx;
+        border-radius: 10rpx;
+        margin-bottom: 20rpx;
+        background-color: #fff;
+        box-shadow: 0 2rpx 10rpx rgba(0, 0, 0, 0.03);
+        border-left: 6rpx solid transparent;
         
-        .mode-text {
-          color: #fff;
+        text {
+          font-size: 30rpx;
+          color: #666;
         }
-      }
-      
-      .mode-text {
-        font-size: 28rpx;
-        color: #666;
+        
+        .mode-desc {
+          font-size: 24rpx;
+          color: #999;
+          margin-top: 10rpx;
+        }
+        
+        &.active {
+          border-left-color: #5b6af0;
+          
+          text:first-child {
+            color: #5b6af0;
+            font-weight: 500;
+          }
+        }
       }
     }
   }
