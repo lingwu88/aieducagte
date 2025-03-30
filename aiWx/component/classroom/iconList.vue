83adc1d7eb0950bf216fd8429c411a8de4e2b327
PennLam <lemonmonlucy@163.com>
2025-03-30 13:43:40 +0800

    美化课堂页面

--- aiWx/component/classroom/iconList.vue
+++ aiWx/component/classroom/iconList.vue
@@ -2,7 +2,9 @@
 	<view class="content">
     <view class="list">
       <view class="list-item" v-for="(item,index) in menu" :key="index" @click="handleTo(item)">
-        <u-image :src="item.img" :lazy-load="true" height="120rpx" width="120rpx"></u-image>
+        <view class="icon-container">
+          <u-image :src="item.img" :lazy-load="true" height="80rpx" width="80rpx"></u-image>
+        </view>
         <text class="text" selectable="false">{{ item.content }}</text>
       </view>
     </view>
@@ -71,13 +73,36 @@
         display: flex;
         flex-direction: column;
         align-items: center;
-        width: 120rpx;
+        width: 140rpx;
+        margin: 0 10rpx;
+
+        .icon-container {
+          width: 120rpx;
+          height: 120rpx;
+          background: rgba(255, 255, 255, 0.2);
+          backdrop-filter: blur(10px);
+          -webkit-backdrop-filter: blur(10px);
+          border-radius: 20rpx;
+          box-shadow: 0 8rpx 32rpx 0 rgba(91, 106, 240, 0.1);
+          border: 1px solid rgba(255, 255, 255, 0.18);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          transition: all 0.3s ease;
+          
+          &:active {
+            transform: scale(0.95);
+            box-shadow: 0 4rpx 16rpx 0 rgba(91, 106, 240, 0.15);
+          }
+        }
 
         .text{
           font-size: 30rpx;
           margin: 25rpx 0;
           text-align: center;
           width: 100%;
+          color: #333;
+          font-weight: 500;
         }
       }
     }
